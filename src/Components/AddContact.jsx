7e345863f13f87797d612aa3dfb6e70d2d07e522
Lamvo27072001 <lampham27072001@gmail.com>
2022-07-11 01:10:06 +0700

    4. Study how to use library outside and React Structure

--- src/Components/AddContact.jsx
+++ src/Components/AddContact.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 
 export default function AddContact() {
   return (
-    <div className='ui main'>
+    <div className='add_contact'>
         <h2>Add Contact</h2>
-        <form className="ui form">
-            <div className='field'>
+        <form className="ui_form">
+            <div className='field '>
                 <label htmlFor="">Name</label>
                 <input type="text" name='name' placeholder='Name' />
             </div>
+            <div className='field '>
+                <label htmlFor="">Email</label>
+                <input type="email" name='email' placeholder='Email' />
+            </div>
+            <button className='ui button red'>Add</button>
         </form>
     </div>
   )
